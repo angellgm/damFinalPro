76d38756f96e26aff3f5ed14ad96756ef5d35975
Angellgm <angelgmcorreo@gmail.com>
2021-04-21 08:44:07 +0200

    Inicial

---
+++ desktop/src/com/algm/sck/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.algm.sck.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.algm.sck.SarsCovKiller;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SarsCovKiller(), config);
+	}
+}
