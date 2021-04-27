a2ef71fa2bb7406783d1f3c7ea1e98fff2bf9d8f
Angellgm <angelgmcorreo@gmail.com>
2021-04-27 22:44:12 +0200

    test movimiento

--- core/src/com/algm/sck/SarsCovKiller.java
+++ core/src/com/algm/sck/SarsCovKiller.java
@@ -1,5 +1,6 @@
 package com.algm.sck;
 
+import com.algm.movimiento.MovNave;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,27 +9,40 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SarsCovKiller extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	Texture fondo;
+	Texture imgNave;
+	MovNave nave;
+ 
+
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		imgNave = new Texture("nave.png");
+		nave = new MovNave();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		if (nave.getNavePosicion()<0) {
+			nave.setNaveDireccion(1);
+		}
+		
+		if (nave.getNavePosicion()>500) {
+			nave.setNaveDireccion(-1);
+		}
+		int navep=nave.getNavePosicion();
+		nave.setNavePosicion(navep += (3 * nave.getNaveDireccion()));
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(imgNave, nave.getNavePosicion(), 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		imgNave.dispose();
 	}
 }
